726fa65dfd35724a5fc8180ce8628a8f283ffff9
TradeGuru <tradeguru.net@gmail.com>
2026-05-14 04:33:59 +0530

    Fix: Socket connection

--- get_nifty_ids.py
+++ get_nifty_ids.py
@@ -1,7 +1,7 @@
 from dhanhq import marketfeed
 
-CLIENT_ID = "your_client_id_here"
-ACCESS_TOKEN = "your_fresh_token_here"
+CLIENT_ID = "1103060314"
+ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJkaGFuIiwicGFydG5lcklkIjoiIiwiZXhwIjoxNzc4NzkzMTU0LCJpYXQiOjE3Nzg3MDY3NTQsInRva2VuQ29uc3VtZXJUeXBlIjoiU0VMRiIsIndlYmhvb2tVcmwiOiIiLCJkaGFuQ2xpZW50SWQiOiIxMTAzMDYwMzE0In0.h6f0FsjRIvmWYZ3f-npARoIxsYj8xjudx6EOUqMrF7BeDYhT90tDEjJwUWQ8EOmkDIb7yYugPTgYF0EoRED_PA"
 CE_ID = "51348"
 PE_ID = "51349"
 
